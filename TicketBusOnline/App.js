8efab3d52954ef437001536cfdfd8443aeeb17a2
Bruno Romero <65194372+bruno290300@users.noreply.github.com>
2023-09-27 17:23:04 -0300

    add changes

---
+++ TicketBusOnline/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Mainpage from './src/components/Mainpage';
+import Profile from './src/components/Profile';
+import Login  from './src/components/Login';
+import Register from './src/components/Register';
+import MyTrips from './src/components/My_trips';
+import FindTrips from './src/components/Find_trips';
+
+
+export default function App() {
+  return <Profile/>;
+}
+
+
+
