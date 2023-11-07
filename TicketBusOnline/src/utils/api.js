4f7db13cecb5d3b61792c53c6fcc4cb51de97456
Douglas <douglasarenas71@gmail.com>
2023-11-07 15:35:44 -0300

    Fix API baseURL in api.js

--- TicketBusOnline/src/utils/api.js
+++ TicketBusOnline/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const Instance = axios.create({
-//   baseURL: 'http://localhost:9000/',
-// });
+export const Instance = axios.create({
+  baseURL: 'http://localhost:9000/',
+});
 
 export const baseURL = 'http://192.168.0.107:9000/';
